493bfd74cb9c6d7264a4f7a059986f3466c42460
Haider Mansoor <haider@bitfactory.io>
2023-03-08 13:28:36 +0200

    Incedo-Task

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config()
 const cors = require('cors');
-
+const app = express();
 const artist = require('./routes/artistsRoute');
 const users = require('./routes/userRoutes');
 
-const app = express();
-app.use(express.json())
-app.use(cors())
 const port = process.env.PORT || 3000;
 
+app.use(express.json())
+app.use(cors())
 app.use('/artist/', artist);
 app.use('/users/', users);
 
